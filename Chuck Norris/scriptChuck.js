ca8bf3c42032f9ee447cd2081cbac4401f4b7326
Marine <marine.lenoir@hotmail.fr>
2022-03-21 14:47:15 +0100

    feat: add Chuck

---
+++ Chuck Norris/scriptChuck.js
@@ -0,0 +1,18 @@
+async function chuck(){
+    try{
+    let response = await fetch("https://api.chucknorris.io/jokes/random")
+    let result = await response.json()
+    console.log(result.url)
+    }
+    catch{
+    (error) => alert("Erreur" + error)
+}
+}
+
+
+//.then(response => console.log(response.text()))
+//.then(value => console.log(value))
+
+chuck()
+
+
