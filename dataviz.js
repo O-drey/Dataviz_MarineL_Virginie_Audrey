6a16ef7fd920ababdb95d4ee72c564d092ef69ea
O-drey <98774051+O-drey@users.noreply.github.com>
2022-03-24 17:10:34 +0100

    fix htmls css js + feat pexels

--- dataviz.js
+++ dataviz.js
@@ -7,38 +7,8 @@ async function meteo(){
         let result = await response.json()
         //console.log(result);
         //console.log(result.weather[0].icon)
-        document.getElementById("weather-img").setAttribute("src", "http://openweathermap.org/img/w/"+icon+".png")
-        console.log(result.name)
-        console.log(result.main.temp)
-        const icon = result.weather[0].icon
-        document.getElementById("weather-img").setAttribute("src", "http://openweathermap.org/img/w/"+icon+".png")
-        //console.log("http://openweathermap.org/img/w/"+icon+".png")
-    }catch{
-        (error => alert("Erreur : " + error))
-    }
-}
-
-meteo()
-
-//async function pexels() {
-  //  try{
-    //    let response = await fetch()
-    //    let res = await response.json()
-        //console.log(res)
-        //console.lorg(res.img)
-    //    let background = 
-    //}catch{}
-//}
-
-
-async function meteo(){
-    try{
-        let response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=9dc1ac654e99ad5e689b88fde5a5fd33&units=metric") //key : d9eb38796032a8eb1c62b19c226720c8
-        let result = await response.json()
-        //console.log(result);
-        //console.log(result.weather[0].icon)
-        console.log(result.name)
-        console.log(result.main.temp)
+        //console.log(result.name)
+        //console.log(result.main.temp)
         //let weatherIcon = document.querySelector('#weather-img')
         //console.log(weatherIcon)
         const city = result.name;
@@ -59,7 +29,7 @@ async function meteo(){
 //Pexelskey: 563492ad6f91700001000001608e9238674748009498eea15e94bde2//
 
 
-async function photos(){
+async function photo(){
     //let data = {
        // url : "https://api.pexels.com/v1/",
        // headers: {
@@ -67,27 +37,87 @@ async function photos(){
         //}
     //}
 
+
+
+
     //fetch("https://api.pexels.com/v1/", {
    // method: 'GET',
     //headers: {
      //   'Authorization' : '563492ad6f91700001000001608e9238674748009498eea15e94bde2' //process.env(API_KEY)
     //}
+    //try{
+        //let resp = await fetch("https://api.pexels.com/v1/")
+       // let resultat = await resp.json()
+        //console.log(resultat)
+   // }
+
+    //catch(error) {alert("Erreur : " + error)}
+
+
+
+
+
     //.then(resp => {
       //  return resp.json()
     //})
     //.then(data => console.log(data))
 
-    try{
-        let resp = await fetch("https://api.pexels.com/v1/")
-        let resultat = await resp.json()
-        console.log(resultat)
+    
+
+
+    
+console.log(photo)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const client = createClient('563492ad6f91700001000001608e9238674748009498eea15e94bde2');
+let photos = url("https://api.pexels.com/v1/")
+const query = 'Weather';
+
+client.photos.search({ query, per_page: 1 })
+    .then(photos => 
+        {
+            photos.json()
+    })
+    .then(data => 
+        console.log(data))
+    .catch(err => console.log("error",err.message))
+
     }
 
-    catch(error) {alert("Erreur : " + error)}
 
-}
-    
-console.log(client)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -95,4 +125,4 @@ console.log(client)
 
 
 meteo()
-photos()
+photo()
