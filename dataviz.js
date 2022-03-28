a12c74a81cbe432ad26b748101ec5c5d6957de82
Marine <marine.lenoir@hotmail.fr>
2022-03-28 10:13:18 +0200

    Feat: JS Pexels API

--- dataviz.js
+++ dataviz.js
@@ -30,98 +30,19 @@ async function meteo(){
 
 
 async function photo(){
-    //let data = {
-       // url : "https://api.pexels.com/v1/",
-       // headers: {
-            //'Authorization' : '563492ad6f91700001000001608e9238674748009498eea15e94bde2'
-        //}
-    //}
-
-
-
-
-    //fetch("https://api.pexels.com/v1/", {
-   // method: 'GET',
-    //headers: {
-     //   'Authorization' : '563492ad6f91700001000001608e9238674748009498eea15e94bde2' //process.env(API_KEY)
-    //}
-    //try{
-        //let resp = await fetch("https://api.pexels.com/v1/")
-       // let resultat = await resp.json()
-        //console.log(resultat)
-   // }
-
-    //catch(error) {alert("Erreur : " + error)}
-
-
-
-
-
-    //.then(resp => {
-      //  return resp.json()
-    //})
-    //.then(data => console.log(data))
-
-    
-
-
-    
-console.log(photo)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const client = createClient('563492ad6f91700001000001608e9238674748009498eea15e94bde2');
-let photos = url("https://api.pexels.com/v1/")
-const query = 'Weather';
-
-client.photos.search({ query, per_page: 1 })
-    .then(photos => 
-        {
-            photos.json()
-    })
-    .then(data => 
-        console.log(data))
-    .catch(err => console.log("error",err.message))
-
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ 
+    fetch("https://api.pexels.com/v1/search?query=people",{
+        headers: {
+          Authorization: "563492ad6f91700001000001608e9238674748009498eea15e94bde2"
+        }
+      })
+         .then(resp => {
+           return resp.json()
+         })
+         .then(data => {
+           console.log(data)
+         })
+        }
 
 
 meteo()
