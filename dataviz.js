b50ffff219fbb671853f7391f4c83d7133851227
Marine <marine.lenoir@hotmail.fr>
2022-03-29 12:29:55 +0200

    Feat: JS Lien 2 API

--- dataviz.js
+++ dataviz.js
@@ -5,12 +5,7 @@ async function meteo(){
     try{
         let response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=9dc1ac654e99ad5e689b88fde5a5fd33&units=metric") //key : d9eb38796032a8eb1c62b19c226720c8
         let result = await response.json()
-        //console.log(result);
-        //console.log(result.weather[0].icon)
-        //console.log(result.name)
-        //console.log(result.main.temp)
-        //let weatherIcon = document.querySelector('#weather-img')
-        //console.log(weatherIcon)
+
         const city = result.name;
         document.getElementById("ville").innerHTML = city;
 
@@ -18,6 +13,7 @@ async function meteo(){
         document.getElementById("temperature").innerHTML = Math.round(temp) + "°C"; 
 
         const icon = result.weather[0].icon
+        console.log(icon)
         document.getElementById("weather-img").setAttribute("src", "http://openweathermap.org/img/w/"+icon+".png")
         //console.log("http://openweathermap.org/img/w/"+icon+".png")
     }catch{
@@ -27,17 +23,12 @@ async function meteo(){
 
 //Pexelskey: 563492ad6f91700001000001608e9238674748009498eea15e94bde2//
 
-//chrome.runtime.onMessage.addListener(
+
 
 async function photo(){
-  // app.use(function(req, res, next) {
-  //   res.header("Access-Control-Allow-Origin", "*");
-  //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  //   next();
-  //   });
     fetch("https://api.pexels.com/v1/search?query=weather",{
         headers: {
-          Origin : "*", //"https://api.pexels.com/v1/search?query=weather",
+          Origin : "*", 
           'Content-Type':"image/jpeg",
           Authorization: "563492ad6f91700001000001608e9238674748009498eea15e94bde2",
         }
@@ -46,14 +37,24 @@ async function photo(){
            return resp.json()
          })
          .then(data => {
-           //let i = 0
-           const url = data.photos[0].src.original
+           const url = data.photos[0].src.landscape
            document.getElementById("background-img").setAttribute("src", url)
-           //document.getElementById("bg").css("background-image",url)//.style.backgroundImage = 'https://images.pexels.com/photos/1118873/pexels-photo-1118873.jpeg'
            console.log(data.photos)
          })
         }
-//)
+
+
+// //let array = [
+//   "01d" = "#FFFF00",
+//   "02d" = "#FFFF00",
+//   "03d" = "#D3D3D3",
+//   "04d" = "#696969",
+//   "09d" = "#4682B4",
+//   "10d" = "#4682B4",
+//   "11d" = "#191970",
+//   "13d" = "#FFFAFA",
+//   "50d" = "#F5F5F5"
+// ]
 
 
 meteo()
